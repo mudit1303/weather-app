bd65b1bddd796087e7514d5734d4e42efd03fc67
mudit1303 <80175458+mudit1303@users.noreply.github.com>
2022-05-30 12:30:31 +0530

    input field implemented

---
+++ weather-app/src/components/search/SearchMain.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import './searchmain.css'
+
+function SearchMain() {
+
+  const [cityName,setCityName] = useState('')
+
+  const handleSearchClick = (e) =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=680c7d7a6fb668232ceac51be84cb2c5`)
+    .then(res => res.json())
+    .then(result => console.log(result))
+    .catch(err=> console.log(err))
+  }
+
+  const handleSearchInput = (e) =>{
+      setCityName(e.target.value)
+  }
+
+  return (
+    <div className='wrap'>
+        <div className='search'>
+            <input type="search" placeholder='city..' id="search" onChange={handleSearchInput}></input>
+        </div>
+        <button className='searchbutton' onClick={handleSearchClick}>
+            Search    
+        </button>
+    </div>
+  )
+}
+
+export default SearchMain
